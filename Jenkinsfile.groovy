bfde4683192c436a19756eda9d396d5a6cebcb7f
Victor Arbues <arbuesv@TMGM-C02TNDTMG8WN.local>
2018-03-20 13:36:40 +0000

    cleanup

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -60,44 +60,4 @@ ansiColor('xterm') {
             }
         }
     }
-    lock("${env.PROJECT_NAME}-prod") {
-     node ("master"){
-
-            def sbtFolder          = "${tool name: 'sbt-0.13.13', type: 'org.jvnet.hudson.plugins.SbtPluginBuilder$SbtInstallation'}/bin"
-            def projectName        = "${env.PROJECT_NAME}"
-            def github_token       = "${env.GITHUB_TOKEN}"
-            def jenkins_github_id  = "${env.JENKINS_GITHUB_CREDENTIALS_ID}"
-            def docker_account     = "${env.AWS_ECR_DOCKER_ACCOUNT}"
-            def docker_registry    = "${env.AWS_ECR_DOCKER_REGISTRY}"
-            def pipeline_version   = "1.0.0-b${env.BUILD_NUMBER}"
-            def github_commit      = ""
-            def slackToken         = "${env.SLACK_PLATFORMS_RELEASES}"
-
-
-            validateProdDeployStage {
-                project_name    = projectName
-                github_token    = github_token
-                slack_channel   = "#platforms-releases"
-                commit_filter   = "PLAT"
-                slack_token     = slackToken
-            }
-            
-            
-            stage("Prod Deploy"){
-                def projEnv = "prod"
-
-                nodejs('Node7.7.2') {
-                  sh """
-                    nStack setup --app-name ${projectName} --app-version ${pipeline_version} --app-env ${projEnv}
-                  """
-                }
-            }
-
-              generateReleaseNotesStage {
-                project_name    = projectName
-                github_token    = github_token
-                release_version = pipeline_version
-            }
-        }
-    }
 }
